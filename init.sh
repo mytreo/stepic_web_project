3a574f90f08fd61f702a41a6cbdf15a8f531fde5
mytreo <mytreogm@gmail.com>
2016-03-09 00:12:07 +0200

    some change

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 #bash /home/box/web/init.sh
 sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
