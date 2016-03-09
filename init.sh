7588c633555f805c1fe4595f69605a396e140943
mytreo <mytreogm@gmail.com>
2016-03-09 19:25:05 +0200

    ok2

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #git clone https://github.com/mytreo/stepic_web_project.git /home/box/web
 #bash /home/box/web/init.sh
 #sudo rm -rf /etc/nginx/sites-enabled/default
+#gunicorn -b 0.0.0.0:8080 hello:app -D
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
