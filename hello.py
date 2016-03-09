cebf00c17cdb9289722faeb4971a60399c8bdcc9
mytreo <mytreogm@gmail.com>
2016-03-09 19:21:06 +0200

    ok

--- hello.py
+++ hello.py
@@ -2,9 +2,6 @@ def app(environ, start_response):
 	resp = environ['QUERY_STRING'].split("&")
 	resp = [item+"\n" for item in resp]
 	status = '200 OK'
-	headers = [
-		('Content-Type', 'text/plain')
-	]
-	body = resp
+	headers = [('Content-Type', 'text/plain')]
 	start_response(status, headers)
-	return [ body ]
+	return  resp; 
