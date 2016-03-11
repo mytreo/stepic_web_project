9695fac08ad8b6577ed78da79cade6d30901e3a0
mytreo <mytreogm@gmail.com>
2016-03-11 23:29:27 +0200

    comment django

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 #bash /home/box/web/init.sh
 #sudo rm -rf /etc/nginx/sites-enabled/default
 #gunicorn -b 0.0.0.0:8080 hello:app -D
+#cd ask
+#gunicorn ask.wsgi:application --bind 0.0.0.:8000
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
